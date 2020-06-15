ffea004d653ec447d7ee9ec5ddb91107a1149e1e
nancychen <beekingdom0201@gmail.com>
2020-06-15 16:19:04 +0800

    ajax第二部分

---
+++ app2/query.php
@@ -0,0 +1,12 @@
+<?php
+//處理查詢資料的請求
+include_once "base.php";
+
+$str=$_GET['str'];
+$db=new DB("students");
+$query=$db->all([]," where name like '%$str%'");
+foreach($query as $q){
+    echo "<div>結果一:".$q['name']."-".$q['class_num']."-".$q['dept']."</div>";
+}
+
+?> 
